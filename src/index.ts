fae8e202be72f70fa3c845fef7da79a3e2e2db86
Bence Fekete <fekete954@gmail.com>
2021-09-23 23:17:13 +0100

    Move constants

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 (function() {
-  type AllowedElement =  'a' | 'article' | 'aside' | 'details' | 'b' | 'br' | 'caption' | 'dialog' | 'div' | 'footer' | 'form' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'header' | 'img' | 'label' | 'li' | 'nav' | 'main' | 'ol' | 'p' | 'section' | 'span' | 'strong' | 'table' | 'tbody' | 'td' | 'tfoot' | 'th' | 'thead' | 'tr' | 'ul'
-  const allowedElementList: AllowedElement[] = ['a', 'article', 'aside', 'details', 'b', 'br', 'caption', 'dialog', 'div', 'footer', 'form', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'header', 'img', 'label', 'li', 'nav', 'main', 'ol', 'p', 'section', 'span', 'strong', 'table', 'tbody', 'td', 'tfoot', 'th', 'thead', 'tr', 'ul']
-
   }
 
     }
